1a270bccacb502532e0671b9f29c40a7c95482e9
Gaspar Chefdeville <gaspar.chefdeville@gmail.com>
2021-07-07 04:18:24 +0200

    implement sr-out-duration

--- script.js
+++ script.js
@@ -26,16 +26,19 @@ document.querySelectorAll("[class^='sr-'], [class*=' sr-']").forEach(element => 
 
         function applyChanges(out=false) {
             let [sr_, visible_, transform_] = out ? [sr.out, visibleOut, transformOut] : [sr, visible, transform];
+
             if (sr_.slide) {
                 let slide = (sr_.distance || slideDefault) * (out ? -1 : 1);
                 let translateX = sr_.slide.includes('left') ? slide : sr_.slide.includes('right') ? -slide : 0;
                 let translateY = sr_.slide.includes('up') ? slide : sr_.slide.includes('down') ? -slide : 0;
                 transform_.push(visible_ ? 'translate(0)' : 'translate(' + translateX + 'px, ' + translateY + 'px)');
             }
+            
             if (sr_.zoom) {
-                let ratio = 0.5 * (out ? 1 : -1);
-                transform_.push(visible_ ? 'scale(1)' : 'scale(' + (1 + (sr_.zoom.includes('in') ? ratio : sr_.zoom.includes('out') ? -ratio : 0 )) + ')');
+                let ratio = 0.5 * (out ? -1 : 1);
+                transform_.push(visible_ ? 'scale(1)' : 'scale(' + (1 + (sr_.zoom.includes('in') ? -ratio : sr_.zoom.includes('out') ? ratio : 0 )) + ')');
             }
+            
             if (sr_.flip) {
                 let angle = 90 * (out ? -1 : 1);
                 let angleX = sr_.flip.includes('down') ? angle : sr_.flip.includes('up') ? -angle : 0;
@@ -43,6 +46,12 @@ document.querySelectorAll("[class^='sr-'], [class*=' sr-']").forEach(element => 
                 angleX && transform_.push('perspective(1500px) ' + (visible_ ? 'rotateX(0)' : 'rotateX(' + angleX + 'deg)'));
                 angleY && transform_.push('perspective(1500px) ' + (visible_ ? 'rotateY(0)' : 'rotateY(' + angleY + 'deg)'));
             }
+            
+            if (!init) {
+                let duration = !isNaN(sr_.duration?.[0]) ? sr_.duration[0] : durationDefault;
+                element.style.transition = 'opacity ' + duration + 'ms,  transform ' + duration + 'ms cubic-bezier(0,.7,.68,1.17)'; // or ease-in-out
+            }
+            
             element.style.transform = transform_.join(' ');
             element.style.opacity = +visible_;
         }
@@ -59,8 +68,6 @@ document.querySelectorAll("[class^='sr-'], [class*=' sr-']").forEach(element => 
     }
 
     toogleReveal(true).then(() => { // initialize style then add animation (for animation onload)
-        let duration = !isNaN(sr.duration?.[0]) ? sr.duration[0] : durationDefault;
-        element.style.transition = 'opacity ' + duration + 'ms,  transform ' + duration + 'ms cubic-bezier(0,.7,.68,1.17)'; // or ease-in-out
         toogleReveal();
         window.addEventListener('scroll', () => toogleReveal());
     })
